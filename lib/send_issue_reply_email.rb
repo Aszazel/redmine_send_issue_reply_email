4f41b18974956b4901ea5894d212c0f04d2d7be5
Lorenz Bara <121941166+Aszazel@users.noreply.github.com>
2025-06-26 07:05:16 +0000

    Reload patches on prepare

--- lib/send_issue_reply_email.rb
+++ lib/send_issue_reply_email.rb
@@ -4,12 +4,18 @@ module SendIssueReplyEmail
   def self.root
     @root ||= Pathname.new File.expand_path('..', File.dirname(__FILE__))
   end
+
+  def self.load_patches
+    Dir[File.join(root.to_s, 'app', 'patches', '**', '*_patch.rb')].each do |f|
+      require_dependency f
+    end
+  end
 end
 
-Rails.configuration.to_prepare do
-  # Load patches for Redmine
-  Dir[SendIssueReplyEmail.root.join('app/patches/**/*_patch.rb')].each {|f| require_dependency f }
-end
+
+# Load patches at startup and reload them on each prepare
+SendIssueReplyEmail.load_patches
+Rails.configuration.to_prepare { SendIssueReplyEmail.load_patches }
 
 # Load hooks
 Dir[SendIssueReplyEmail.root.join('app/hooks/*_hook.rb')].each {|f| require_dependency f }
